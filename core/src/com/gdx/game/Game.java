a684bfcb1258b4323ae9d95b6414dc7eaf984f58
p-r-o-m-e <97275566+p-r-o-m-e@users.noreply.github.com>
2022-05-16 22:30:24 +0530

    created and cleaned a blank window, created some sprites

--- core/src/com/gdx/game/Game.java
+++ core/src/com/gdx/game/Game.java
@@ -1,31 +1,68 @@
 package com.gdx.game;
 
 import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.audio.Music;
+import com.badlogic.gdx.audio.Sound;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.ScreenUtils;
 
+import org.w3c.dom.Text;
+import com.badlogic.gdx.math.Rectangle;
+
 public class Game extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
-	
+
+	public Texture dropIMG;
+	public Texture bucketIMG;
+	public Sound dropSound;
+	public Music rainMusic;
+
+	public SpriteBatch batch;
+	public OrthographicCamera camera;
+
+	private Rectangle bucket;
+
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		bucket = new Rectangle();
+		bucket.x = 800/2 - 64/2;
+		bucket.y = 20;
+		bucket.width = 64;
+		bucket.height = 64;
+
+		camera = new OrthographicCamera();
+		camera.setToOrtho(false, 800, 480);
+
+		//LOAD IMAGES FOR DROPLET & BUCKET.
+		dropIMG = new Texture(Gdx.files.internal("drop.png"));
+		bucketIMG = new Texture(Gdx.files.internal("bucket.png"));
+		//LOAD SOUND
+		dropSound = Gdx.audio.newSound(Gdx.files.internal("drop.wav"));
+		rainMusic = Gdx.audio.newMusic(Gdx.files.internal("rain.mp3"));
+
+		//set the playback of the BGM immediately.
+		rainMusic.setLooping(true);
+		rainMusic.play();
 	}
 
 	@Override
 	public void render () {
-		ScreenUtils.clear(1, 0, 0, 1);
+		ScreenUtils.clear(0, 0, 0.2f, 1);
+
+		batch.setProjectionMatrix(camera.combined);
 		batch.begin();
-		batch.draw(img, 0, 0);
+		batch.draw(bucketIMG, bucket.x, bucket.y);
 		batch.end();
+
+		camera.update();
 	}
 	
 	@Override
 	public void dispose () {
-		batch.dispose();
-		img.dispose();
+//		batch.dispose();
+//		img.dispose();
 	}
 }
