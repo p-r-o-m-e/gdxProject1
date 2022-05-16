67ce6416116a6b362c5702b2529063c7e51a4802
p-r-o-m-e <97275566+p-r-o-m-e@users.noreply.github.com>
2022-05-16 22:01:05 +0530

    set screen resolution, title, disabled some unnecessary functionalities

--- desktop/src/com/gdx/game/DesktopLauncher.java
+++ desktop/src/com/gdx/game/DesktopLauncher.java
@@ -8,6 +8,9 @@ import com.gdx.game.Game;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+		config.setTitle("Drop");
+		config.setWindowedMode(800,480);
+		config.useVsync(true);
 		config.setForegroundFPS(60);
 		new Lwjgl3Application(new Game(), config);
 	}
